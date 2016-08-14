19a90c8ae14d662b067e7ea2a6f66fc4a88ac96a
Valerie Gibson <vgibson14@gmail.com>
2016-08-14 23:15:30 +0100

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           return Content("yo");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace FirstApp
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+             app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+    }
+}
